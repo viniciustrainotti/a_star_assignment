31e9659cefbb92db38d910ae79a356dd28e46a20
Vinícius Trainotti <vtrainotti@live.com>
2022-03-28 22:40:16 -0300

    primeiro refactor

--- a_star.py
+++ a_star.py
@@ -1,93 +1,140 @@
 import math
-from dists import dists
+from dists import dists, straight_line_dists_from_bucharest
 
-from modelo import *
 from copy import deepcopy
-from matplotlib import pyplot
-from numpy import random
 
 import sys
 
+class Node():
+	#Classe que representa um nó (cidade)
+
+	def __init__(self, nome_cidade, valor_heuristico):
+		self.cidade = nome_cidade
+		self.heuristica = valor_heuristico
+		self.vizinhos = []
+
+	def __init__(self, nome_cidade):
+		self.cidade = nome_cidade
+		self.heuristica = 0
+		self.vizinhos = []
+
+	def set_heuristica(self, heuristica):
+		self.heuristica = heuristica
+
+	def get_heuristica(self):
+		return self.heuristica
+
+	def get_coordenada(self):
+		return self.coordenada
+
+	def __str__(self):
+		return self.cidade
+
+class Edge():
+	#Classe que representa os Nós filhos (cidades vizinhas)
+
+	def __init__(self, node_alvo, valor_custo):
+		self.node = node_alvo
+		self.distancia = valor_custo
+
+	def custo(self):
+		return self.distancia + self.node.heuristica
+
+class Route():
+
+	def __init__(self, Node_Inicial):
+		self.distancias = 0
+		#Nodes avaliados
+		self.nodes = [Node_Inicial]
+		#Nodes ainda não avaliados
+		self.ramos = Node_Inicial.vizinhos
+
+	def custo_ramos(self):
+		custo_ramos = []
+		for ramo in self.ramos:
+			custo_ramos.append(self.distancias + ramo.custo())
+
+		return custo_ramos
+
+	def adiciona(self, Ramo):
+		self.distancias += Ramo.distancia
+		self.nodes.append(Ramo.node)
+		self.ramos = Ramo.node.vizinhos
+
+	def retira(self, Ramo):
+		for ramo in self.ramos:
+			if(ramo.node.cidade == Ramo.node.cidade):
+				self.ramos.remove(ramo)
+
+	def __str__(self):
+		nodes = ""
+		for node in self.nodes:
+			nodes += " -> {}".format(node)
+		return "{} - {}".format(nodes, self.distancias)
+
+
 def str_to_Node(classname, Nodes):
-    for node in Nodes:
-        if node.cidade == classname:
-            return node
-    #return getattr(sys.modules[__name__], classname)
+    return next((node for node in Nodes if node.cidade == classname), None)
+    # for node in Nodes:
+    #     if node.cidade == classname:
+    #         return node
 
 def Rota(Node_Inicio, Node_Fim):
 
-	if(Node_Fim.cidade == Node_Inicio.cidade):
-		return [Node_Inicio]
+    if(Node_Fim.cidade == Node_Inicio.cidade):
+        return [Node_Inicio]
 
-	Fila_De_Rotas = []
+    Fila_De_Rotas = []
 
-	rota = Route(Node_Inicio)
-	Fila_De_Rotas.append(rota)
+    rota = Route(Node_Inicio)
+    Fila_De_Rotas.append(rota)
 
 
-	while(True):
-		melhores_ramos = []
-		melhor_ramo = None
+    while(True):
+        melhores_ramos = []
+        melhor_ramo = None
 
-		index_melhores_ramos = []
-		rota_com_melhor_ramo = None
-		
-		#Encontra o melhor ramo entre os ramos abertos
-		for rota in Fila_De_Rotas:
-			custo_ramos = rota.custo_ramos()
-			melhores_ramos.append(min(custo_ramos))
-			index_melhores_ramos.append(custo_ramos.index(min(custo_ramos)))
+        index_melhores_ramos = []
+        rota_com_melhor_ramo = None
 
-		indice_melhor_rota = melhores_ramos.index(min(melhores_ramos))
-		rota_com_melhor_ramo = deepcopy(Fila_De_Rotas[indice_melhor_rota])
-		melhor_ramo = rota_com_melhor_ramo.ramos[index_melhores_ramos[indice_melhor_rota]]
-	
-		#Remove ramo da rota antiga
-		rota = Fila_De_Rotas[indice_melhor_rota]
-		rota.retira(melhor_ramo)
-		
-		rota_com_melhor_ramo.adiciona(melhor_ramo)
-		Fila_De_Rotas.append(rota_com_melhor_ramo)
+        #Encontra o melhor ramo entre os ramos abertos
+        for rota in Fila_De_Rotas:
+            custo_ramos = rota.custo_ramos()
+            melhores_ramos.append(min(custo_ramos))
+            index_melhores_ramos.append(custo_ramos.index(min(custo_ramos)))
 
-		#Adiciona uma nova rota ou remove uma rota que já acabou
-		if(len(rota.ramos) == 0):
-			Fila_De_Rotas.remove(rota)	
+        indice_melhor_rota = melhores_ramos.index(min(melhores_ramos))
+        rota_com_melhor_ramo = deepcopy(Fila_De_Rotas[indice_melhor_rota])
+        melhor_ramo = rota_com_melhor_ramo.ramos[index_melhores_ramos[indice_melhor_rota]]
 
-		#Condição de parada
-		print(rota_com_melhor_ramo)
-		if(melhor_ramo.node.cidade == Node_Fim.cidade):
-			break;
+        #Remove ramo da rota antiga
+        rota = Fila_De_Rotas[indice_melhor_rota]
+        rota.retira(melhor_ramo)
 
-	return rota_com_melhor_ramo.nodes
+        rota_com_melhor_ramo.adiciona(melhor_ramo)
+        Fila_De_Rotas.append(rota_com_melhor_ramo)
 
-# goal sempre sera 'bucharest'
+        #Adiciona uma nova rota ou remove uma rota que já acabou
+        if(len(rota.ramos) == 0):
+            Fila_De_Rotas.remove(rota)	
+
+        #Condição de parada
+        print(rota_com_melhor_ramo)
+        if(melhor_ramo.node.cidade == Node_Fim.cidade):
+            break;
+
+    return rota_com_melhor_ramo.nodes
+
+# goal sempre sera 'Bucharest'
 def a_star(start, goal='Bucharest'):
     """
     Retorna uma lista com o caminho de start até 
     goal segundo o algoritmo A*
     """
-    Bucharest = Node("Bucharest")
-    Vaslui = Node("Vaslui")
-    Eforie = Node("Eforie")
-    Oradea = Node("Oradea")
-    Hirsova = Node("Hirsova")
-    Zerind = Node("Zerind")
-    Iasi = Node("Iasi")
-    Giurgiu = Node("Giurgiu")
-    Arad = Node("Arad")
-    Urzineci = Node("Urzineci")
-    Fagaras = Node("Fagaras")
-    Sibiu = Node("Sibiu")
-    Rimnicu_Vilcea = Node("Rimnicu_Vilcea")
-    Craiova = Node("Craiova")
-    Dobreta = Node("Dobreta")
-    Mehadia = Node("Mehadia")
-    Neamt = Node("Neamt")
-    Lugoj = Node("Lugoj")
-    Timisoara = Node("Timisoara")
-    Pitesti = Node("Pitesti")
-
-    Nodes = [Bucharest, Vaslui, Eforie, Pitesti, Lugoj, Oradea, Hirsova, Zerind, Iasi, Giurgiu, Arad, Urzineci, Fagaras, Sibiu, Rimnicu_Vilcea, Craiova, Dobreta, Mehadia, Neamt, Timisoara]
+    Nodes = []
+    cities = list(straight_line_dists_from_bucharest.keys())
+    for city in cities:
+            Nodes.append(Node(city))
 
     #Cidades de partida e destino
     Node_Inicio = str_to_Node(start, Nodes)
@@ -95,39 +142,25 @@ def a_star(start, goal='Bucharest'):
 
     #Monta a heuristica de acordo com a cidade destino
     for node in Nodes:
-        node.set_heuristica(node)
-        print("{} - {}".format(node.cidade, node.get_heuristica()))
-
-    #Inicializando vizinhos
-    Arad.vizinhos = [Edge(Zerind, 75), Edge(Sibiu, 140), Edge(Timisoara, 118)]
-    Zerind.vizinhos = [Edge(Oradea, 71), Edge(Arad, 75)]
-    Oradea.vizinhos = [Edge(Zerind, 71), Edge(Sibiu, 151)]
-    Sibiu.vizinhos = [Edge(Oradea, 151), Edge(Arad, 140), Edge(Fagaras, 99), Edge(Rimnicu_Vilcea, 80)]
-    Fagaras.vizinhos = [Edge(Sibiu, 99), Edge(Bucharest, 211)]
-    Rimnicu_Vilcea.vizinhos = [Edge(Sibiu, 80), Edge(Pitesti, 97), Edge(Craiova, 146)]
-    Pitesti.vizinhos = [Edge(Rimnicu_Vilcea, 97), Edge(Bucharest, 101), Edge(Craiova, 138)]
-    Timisoara.vizinhos = [Edge(Arad, 118), Edge(Lugoj, 111)]
-    Lugoj.vizinhos = [Edge(Timisoara, 111), Edge(Mehadia, 70)]
-    Mehadia.vizinhos = [Edge(Lugoj, 70), Edge(Dobreta, 75)]
-    Dobreta.vizinhos = [Edge(Mehadia, 75), Edge(Craiova, 120)]
-    Craiova.vizinhos = [Edge(Dobreta, 120), Edge(Rimnicu_Vilcea, 146), Edge(Pitesti, 138)]
-    Bucharest.vizinhos = [Edge(Fagaras, 211), Edge(Pitesti, 101), Edge(Giurgiu, 90), Edge(Urzineci, 85)]
-    Giurgiu.vizinhos = [Edge(Bucharest, 90)]
-    Urzineci.vizinhos = [Edge(Bucharest, 85), Edge(Hirsova, 98), Edge(Vaslui, 142)]
-    Hirsova.vizinhos = [Edge(Urzineci, 98), Edge(Eforie, 86)]
-    Eforie.vizinhos = [Edge(Hirsova, 86)]
-    Vaslui.vizinhos = [Edge(Urzineci, 142), Edge(Iasi, 92)]
-    Iasi.vizinhos = [Edge(Vaslui, 92), Edge(Neamt, 87)]
-    Neamt.vizinhos = [Edge(Iasi, 87)]
+        node.set_heuristica(straight_line_dists_from_bucharest[node.cidade])
+        # print("{} - {}".format(node.cidade, node.get_heuristica()))
+        for neighboring_cities in dists[node.cidade]:
+            node.vizinhos.append(Edge(str_to_Node(neighboring_cities[0], Nodes), neighboring_cities[1]))
+            # print(f"{neighboring_cities} {neighboring_cities[0]} {neighboring_cities[1]} ")
 
     #Calculando a rota
-    Solucao = Rota(Node_Inicio, Node_Fim)
-
-    return(f"Solução para {start} até {goal}")
+    return Rota(Node_Inicio, Node_Fim)
 
 if __name__ == "__main__":
-    start = sys.argv[1]
+    start = sys.argv[1] if len(sys.argv) > 1 else 'Oradea'
     a_star(start=start)
 
-# Example:
+# Examples input/output:
+# $ python a_star.py 
+# -> Oradea -> Sibiu -> Rimnicu_Vilcea -> Pitesti -> Bucharest - 429
+#
 # $ python a_star.py Oradea
+# -> Oradea -> Sibiu -> Rimnicu_Vilcea -> Pitesti -> Bucharest - 429
+#
+# $ python a_star.py Arad
+# -> Lugoj -> Mehadia -> Dobreta -> Craiova -> Pitesti -> Bucharest - 504
